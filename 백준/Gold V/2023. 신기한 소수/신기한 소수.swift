945c73b8822a34211c8d252b2b82bbae28bee552
Sukyoung Kim <68066104+KSK9820@users.noreply.github.com>
2024-02-15 16:48:25 +0900

    [Gold V] Title: 신기한 소수, Time: 12 ms, Memory: 79516 KB -BaekjoonHub

---
+++ 백준/Gold V/2023. 신기한 소수/신기한 소수.swift
@@ -0,0 +1,74 @@
+import Foundation
+
+let c = Int(readLine()!)!
+let s = Int(pow(10.0, Float(c-1)))
+let e = Int(pow(10.0, Float(c)))
+
+var already = [Int: Bool]()
+var result = [Int]()
+
+func find(n: Int) -> Bool {
+    if n <= 1 { return false }
+    if n == 2 { return true }
+    if n % 2 == 0 { return false }
+    for i in stride(from: 3, through: sqrt(Double(n)), by: 2) {
+        if n % Int(i) == 0 {
+            already[n] = false
+            return false
+        }
+    }
+    already[n] = true
+    return true
+}
+
+
+func separate(_ n : Int) -> [Int] {
+    var numbers = [Int]()
+    var i = s
+    while i != 1 {
+        numbers.append(n / i)
+        i /= 10
+    }
+    numbers.append(n)
+    return numbers
+}
+
+var sel = [Int]()
+var firstNum = [Int]()
+func firstCheck(idx: Int) {
+    if idx == c {
+        firstNum.append(Int(sel.map { String($0) }.joined())!)
+        return
+    }
+    
+    if idx == 0 {
+        for i in [2,3,5,7] {
+            sel.append(i)
+            firstCheck(idx: idx+1)
+            sel.popLast()
+        }
+    } else {
+        for i in [1,3,5,7,9] {
+            sel.append(i)
+            if find(n: Int(sel.map { String($0) }.joined())!) == false {
+                sel.popLast()
+                continue
+            }
+            firstCheck(idx: idx + 1)
+            sel.popLast()
+        }
+    }
+}
+
+firstCheck(idx: 0)
+for i in firstNum {
+    let s = separate(i)
+    var flag = true
+    for j in s {
+        if !find(n: j) {
+            flag = false
+        }
+    }
+    if flag == true { result.append(i) }
+}
+print(result.map { String($0) }.joined(separator: "\n"))
