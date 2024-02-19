a65f6c4a9531f9cb2d52b09f369c0e8a18e3db23
Sukyoung Kim <68066104+KSK9820@users.noreply.github.com>
2024-02-19 15:24:50 +0900

    [Silver II] Title: 어깨동무, Time: 892 ms, Memory: 145404 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/27932. 어깨동무/어깨동무.swift
+++ 백준/Silver/27932. 어깨동무/어깨동무.swift
@@ -4,30 +4,43 @@ let arr = readLine()!.split(separator: " ").map { Int($0)! }
 
 var left = 0
 var right = 1000000000
+
 var result = [Int]()
 var zero = false
 
 while left<=right {
-
-    if arr.count == 1 || Set(arr).count == 1 {
+    if arr.count == 1 {
         zero = true
         break
     }
-    
+
     let mid = (left+right)/2
-    var tired = [Bool](repeating: false, count: n)
-    for i in 0..<n-1 {
-        if abs(arr[i]-arr[i+1]) > mid {
-            tired[i] = true
-            tired[i+1] = true
+
+    var count = 0
+    var tired = [Int]()
+
+    for i in 0..<arr.count {
+        if i == 0 {
+            if abs(arr[i]-arr[i+1]) > mid {
+                tired.append(arr[i])
+            }
+        } else if i == arr.count - 1 {
+            if abs(arr[i]-arr[i-1]) > mid {
+                tired.append(arr[i])
+            }
+        }
+        else {
+            if abs(arr[i] - arr[i-1]) > mid || abs(arr[i]-arr[i+1]) > mid {
+                tired.append(arr[i])
+            }
         }
     }
-    
-    if tired.filter({ $0 == true }).count <= k {
+
+    if tired.count <= k {
         right = mid - 1
     } else {
         left = mid + 1
     }
-    
+
 }
 zero ? print(0) : print(left)
