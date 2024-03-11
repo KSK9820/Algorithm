a815addb5b746266b66ea0064d987a5e96e17be4
Sukyoung Kim <68066104+KSK9820@users.noreply.github.com>
2024-03-11 16:41:10 +0900

    [Gold II] Title: 친구 네트워크, Time: 304 ms, Memory: 92832 KB -BaekjoonHub

--- 백준/Gold/4195. 친구 네트워크/친구 네트워크.swift
+++ 백준/Gold/4195. 친구 네트워크/친구 네트워크.swift
@@ -1,40 +1,44 @@
+
 let t = Int(readLine()!)!
 var result = [Int]()
 for _ in 0..<t {
     let f = Int(readLine()!)!
-    var friend = [String: String]()
-    var count = [String: Int]()
-
+    var index = 0
+    var dict = [String : Int]()
+    var root = [Int](repeating: 1, count: 2 * f)
+    var friend = [Int](0..<(2*f))
     
     for _ in 0..<f {
-        let names = readLine()!.split(separator: " ").map { String($0) }
-        
-        for name in names {
-            if friend[name] == nil {
-                friend[name] = name
-                count[name] = 1
-            }
+        let input = readLine()!.split(separator: " ").map { String($0) }
+        if dict[input[0]] == nil {
+            dict[input[0]] = index
+            index += 1
         }
-        
-        result.append(union(names[0], names[1]))
+        if dict[input[1]] == nil {
+            dict[input[1]] = index
+            index += 1
+        }
+       
+        union(dict[input[0]]!, dict[input[1]]!)
+        result.append(root[find(dict[input[0]]!)])
     }
     
-    func union(_ a: String, _ b: String) -> Int {
+    func union(_ a: Int, _ b: Int) {
         let aP = find(a)
         let bP = find(b)
         
         if aP != bP {
-            friend[bP] = aP
-            count[aP]! += count[bP]!
+            friend[min(aP, bP)] = max(aP, bP)
+            root[max(aP, bP)] += root[min(aP, bP)]
         }
-        return count[aP]!
     }
 
-    func find(_ index: String) -> String {
+    func find(_ index: Int) -> Int {
         if friend[index] != index {
-            friend[index] = find(friend[index]!)
+            friend[index] = find(friend[index])
         }
-        return friend[index]!
+        return friend[index]
     }
+
 }
 print(result.map { String($0) }.joined(separator: "\n"))
