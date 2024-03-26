44b676208fde93d89e3b967aed5790229a865da0
Sukyoung Kim <68066104+KSK9820@users.noreply.github.com>
2024-03-26 14:11:34 +0900

    [Silver I] Title: 스타트링크, Time: 312 ms, Memory: 70088 KB -BaekjoonHub

---
+++ 백준/Silver/5014. 스타트링크/스타트링크.swift
@@ -0,0 +1,48 @@
+
+let fsgud = readLine()!.split(separator: " ").map { Int($0)! }
+let (f, s, g, u, d) = (fsgud[0], fsgud[1], fsgud[2], fsgud[3], fsgud[4])
+var q = Queue<(u: Int, d: Int)>()
+var visited = [Bool](repeating: false, count: f+1)
+var result = -1
+q.enqueue((0,0))
+
+
+if s > g && d == 0 { }
+else if s < g && u == 0 { }
+else {
+    while true {
+        guard let now = q.dequeue() else { break }
+        
+        if now.u * u - now.d * d + s == g {
+            result = now.d + now.u
+            break
+        }
+        
+        if now.u * u - now.d * d + s <= f && now.u * u - now.d * d + s > 0 {
+            if visited[now.u * u - now.d * d + s] { continue }
+            visited[now.u * u - now.d * d + s] = true
+            
+            q.enqueue((now.u+1, now.d))
+            q.enqueue((now.u, now.d + 1))
+        }
+    }
+}
+
+result == -1 ? print("use the stairs") : print(result)
+
+struct Queue<T> {
+    private var input = [T]()
+    private var output = [T]()
+    
+    mutating func enqueue(_ newElement: T) {
+        input.append(newElement)
+    }
+    
+    mutating func dequeue() -> T? {
+        if output.isEmpty {
+            output = input.reversed()
+            input = [T]()
+        }
+        return output.popLast()
+    }
+}
