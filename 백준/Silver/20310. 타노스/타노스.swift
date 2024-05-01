6250c7f81988bb1a7892be40674020b0647243f3
Sukyoung Kim <68066104+KSK9820@users.noreply.github.com>
2024-05-01 15:50:16 +0900

    [Silver III] Title: 타노스, Time: 8 ms, Memory: 69100 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/20310. 타노스/타노스.swift
+++ 백준/Silver/20310. 타노스/타노스.swift
@@ -1,5 +1,19 @@
-let s = readLine()!.map { $0 }
-var zeroCount = s.filter { $0 == "0" }.count
-let zero = [String](repeating: "0", count: zeroCount / 2).joined()
-let one = [String](repeating: "1", count: (s.count - zeroCount) / 2).joined()
-print(zero + one)
+var s = readLine()!.map { String($0) }
+let zeroCount = s.filter { $0 == "0" }.count
+let oneCount = s.count - zeroCount
+
+var tempZero = 0
+var tempOne = 0
+for i in 0..<s.count{
+    if s[i] == "1" && tempOne < (oneCount / 2) {
+        s[i] = ""
+        tempOne += 1
+    }
+}
+for i in (0..<s.count).reversed() {
+    if s[i] == "0" && tempZero < (zeroCount / 2) {
+        s[i] = ""
+        tempZero += 1
+    }
+}
+print(s.joined())
