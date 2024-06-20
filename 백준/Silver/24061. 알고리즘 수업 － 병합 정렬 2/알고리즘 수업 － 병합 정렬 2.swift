7835a6be5277df344132b3abe41846a8654a02c5
Sukyoung Kim <68066104+KSK9820@users.noreply.github.com>
2024-06-20 23:20:58 +0900

    [Silver IV] Title: 알고리즘 수업 - 병합 정렬 2, Time: 720 ms, Memory: 106780 KB -BaekjoonHub

---
+++ 백준/Silver/24061. 알고리즘 수업 － 병합 정렬 2/알고리즘 수업 － 병합 정렬 2.swift
@@ -0,0 +1,61 @@
+func mergeSort(_ array: [Int], _ s: Int, _ e: Int) {
+    if array.count <= 1 { return }
+    
+    if s < e {
+        let m = (s + e) / 2
+        mergeSort(array, s, m)
+        mergeSort(array, m + 1, e)
+        merge(s, m, e)
+    }
+}
+
+func merge(_ s: Int, _ m: Int, _ e: Int) {
+    var i = s
+    var j = m + 1
+    
+    var sortedArray = [Int]()
+    
+    while i <= m && j <= e {
+        if arr[i] <= arr[j] {
+            sortedArray.append(arr[i])
+            i += 1
+        } else {
+            sortedArray.append(arr[j])
+            j += 1
+        }
+    }
+    
+    while i <= m {
+        sortedArray.append(arr[i])
+        i += 1
+    }
+    
+    while j <= e {
+        sortedArray.append(arr[j])
+        j += 1
+    }
+    
+    i = s
+    var t = 0
+    while i <= e {
+        arr[i] = sortedArray[t]
+        count += 1
+        if count == k {
+            answer = arr
+            break
+        }
+        i += 1
+        t += 1
+    }
+}
+                           
+
+let nk = readLine()!.split(separator: " ").map { Int($0)! }
+let (n,k) = (nk[0], nk[1])
+var arr = readLine()!.split(separator: " ").map { Int($0)! }
+
+var count = 0
+var answer = [Int]()
+mergeSort(arr, 0, n-1)
+print(answer == [] ? -1 : answer.map { String($0) }.joined(separator: " "))
+
