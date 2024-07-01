072013e9ef107778b850bf6d5088e1ade4034f9c
Sukyoung Kim <68066104+KSK9820@users.noreply.github.com>
2024-07-02 00:49:38 +0900

    [Silver II] Title: 주식, Time: 2912 ms, Memory: 143024 KB -BaekjoonHub

---
+++ 백준/Silver/11501. 주식/주식.swift
@@ -0,0 +1,29 @@
+let t = Int(readLine()!)!
+var answer = [String]()
+for _ in 0..<t {
+    let n = Int(readLine()!)!
+    let price = readLine()!.split(separator: " ").map { Int($0)! }
+    var sortedPrice = price.sorted(by: <)
+    
+    var result = 0
+    var count = 0
+    var tempPrice = 0
+    var high = price[n-1]
+
+    for i in (0..<n-1).reversed() {
+        if high > price[i] {
+            count += 1
+            tempPrice += price[i]
+        } else {
+            result += high * count - tempPrice
+            count = 0
+            tempPrice = 0
+            high = price[i]
+        }
+    }
+    if price[0] < high {
+        result += high * count - tempPrice
+    }
+    answer.append(String(result))
+}
+print(answer.joined(separator: "\n"))
