d3b4769a39f0633a518a3344575ae8ac1fdee1c5
Sukyoung Kim <68066104+KSK9820@users.noreply.github.com>
2024-11-28 18:38:09 +0900

    [Silver I] Title: 히오스 프로게이머, Time: 52 ms, Memory: 69892 KB -BaekjoonHub

---
+++ 백준/Silver/16564. 히오스 프로게이머/히오스 프로게이머.swift
@@ -0,0 +1,27 @@
+let nk = readLine()!.split(separator: " ").map{ Int($0)! }
+var (n, k) = (nk[0], nk[1])
+var x = [Int](repeating: 0, count: n)
+
+for i in 0..<n {
+    x[i] = Int(readLine()!)!
+}
+
+var left = x.min()!
+var right = x.max()! + k
+var result = 0
+
+while left <= right {
+    let mid = (left + right) / 2
+    var level = 0
+    x.forEach {
+        level += mid - $0 > 0 ? mid - $0 : 0
+    }
+    
+    if level <= k {
+        result = mid
+        left = mid + 1
+    } else {
+        right = mid - 1
+    }
+}
+print(result)
