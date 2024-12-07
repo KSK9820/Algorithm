e05577d036cec6ffe7286dc3c9df18f812e4e466
Sukyoung Kim <68066104+KSK9820@users.noreply.github.com>
2024-12-07 20:28:44 +0900

    [Gold V] Title: 특별한 서빙, Time: 144 ms, Memory: 85408 KB -BaekjoonHub

---
+++ 백준/Gold/27896. 특별한 서빙/특별한 서빙.swift
@@ -0,0 +1,75 @@
+struct Heap<T> {
+    private var arr = [T]()
+    private let compare: (T,T) -> Bool
+    
+    init(arr: [T] = [T](), compare: @escaping (T, T) -> Bool) {
+        self.arr = arr
+        self.compare = compare
+    }
+    
+    mutating func enheap(_ new: T) {
+        arr.append(new)
+        
+        var childIndex = arr.count-1
+        var parentIndex = (childIndex-1)/2
+        
+        while childIndex > 0 && compare(arr[parentIndex], arr[childIndex]) {
+            arr.swapAt(childIndex, parentIndex)
+            childIndex = parentIndex
+            parentIndex = (childIndex-1)/2
+        }
+    }
+    
+    mutating func deheap() -> T? {
+        if arr.isEmpty { return nil }
+        if arr.count == 1 { return arr.removeLast() }
+        
+        let value = arr[0]
+        arr[0] = arr.removeLast()
+        
+        var index = 0
+        while true {
+            let leftChildIndex = index * 2 + 1
+            let rightChildIndex = leftChildIndex + 1
+            
+            var target = index
+            
+            while leftChildIndex < arr.count && compare(arr[target], arr[leftChildIndex]) {
+                target = leftChildIndex
+            }
+            
+            while rightChildIndex < arr.count && compare(arr[target], arr[rightChildIndex]) {
+                target = rightChildIndex
+            }
+            
+            if target == index { break }
+            arr.swapAt(target, index)
+            index = target
+        }
+        
+        return value
+    }
+    
+    var count: Int {
+        arr.count
+    }
+}
+
+let nm = readLine()!.split(separator: " ").map { Int($0)! }
+let (n,m) = (nm[0], nm[1])
+let line = readLine()!.split(separator: " ").map { Int($0)! }
+var h = Heap<Int>(compare: <)
+
+var bulman = 0
+var gazi = 0
+for i in 0..<n {
+    bulman += line[i]
+    h.enheap(line[i])
+    
+    while bulman >= m {
+        guard let s = h.deheap() else { break }
+        bulman -= 2 * s
+        gazi += 1
+    }
+}
+print(gazi)
