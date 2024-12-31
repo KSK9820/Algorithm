c16b97fdf31e65589a12cbfce54dfa757fe8986d
Sukyoung Kim <68066104+KSK9820@users.noreply.github.com>
2024-12-31 23:33:05 +0900

    [Silver III] Title: N번째 큰 수, Time: 268 ms, Memory: 122024 KB -BaekjoonHub

---
+++ 백준/Silver/2075. N번째 큰 수/N번째 큰 수.swift
@@ -0,0 +1,161 @@
+import Foundation
+
+
+final class FileIO {
+    private let buffer:[UInt8]
+    private var index: Int = 0
+
+    init(fileHandle: FileHandle = FileHandle.standardInput) {
+        
+        buffer = Array(try! fileHandle.readToEnd()!)+[UInt8(0)] // 인덱스 범위 넘어가는 것 방지
+    }
+
+    @inline(__always) private func read() -> UInt8 {
+        defer { index += 1 }
+
+        return buffer[index]
+    }
+
+    @inline(__always) func readInt() -> Int {
+        var sum = 0
+        var now = read()
+        var isPositive = true
+
+        while now == 10
+                || now == 32 { now = read() } // 공백과 줄바꿈 무시
+        if now == 45 { isPositive.toggle(); now = read() } // 음수 처리
+        while now >= 48, now <= 57 {
+            sum = sum * 10 + Int(now-48)
+            now = read()
+        }
+
+        return sum * (isPositive ? 1:-1)
+    }
+
+    @inline(__always) func readString() -> String {
+        var now = read()
+
+        while now == 10 || now == 32 { now = read() } // 공백과 줄바꿈 무시
+
+        let beginIndex = index-1
+
+        while now != 10,
+              now != 32,
+              now != 0 { now = read() }
+
+        return String(bytes: Array(buffer[beginIndex..<(index-1)]), encoding: .ascii)!
+    }
+
+    @inline(__always) func readByteSequenceWithoutSpaceAndLineFeed() -> [UInt8] {
+        var now = read()
+
+        while now == 10 || now == 32 { now = read() } // 공백과 줄바꿈 무시
+
+        let beginIndex = index-1
+
+        while now != 10,
+              now != 32,
+              now != 0 { now = read() }
+
+        return Array(buffer[beginIndex..<(index-1)])
+    }
+}
+
+struct Heap<T> {
+    private var arr = [T]()
+    private let compare: (T,T) -> Bool
+    
+    init(arr: [T] = [T](), compare: @escaping (T, T) -> Bool) {
+        self.arr = arr
+        self.compare = compare
+    }
+    
+    mutating func enheap(_ new: T) {
+        arr.append(new)
+        
+        var childIndex = arr.count-1
+        var parentIndex = (childIndex-1)/2
+        
+        while childIndex > 0 && compare(arr[parentIndex], arr[childIndex]) {
+            arr.swapAt(childIndex, parentIndex)
+            childIndex = parentIndex
+            parentIndex = (childIndex-1)/2
+        }
+    }
+    
+    mutating func deheap() -> T? {
+        if arr.isEmpty { return nil }
+        if arr.count == 1 { return arr.removeLast() }
+        
+        let value = arr[0]
+        arr[0] = arr.removeLast()
+        
+        var index = 0
+        while true {
+            let leftChildIndex = index * 2 + 1
+            let rightChildIndex = leftChildIndex + 1
+            
+            var target = index
+            
+            while leftChildIndex < arr.count && compare(arr[target], arr[leftChildIndex]) {
+                target = leftChildIndex
+            }
+            
+            while rightChildIndex < arr.count && compare(arr[target], arr[rightChildIndex]) {
+                target = rightChildIndex
+            }
+            
+            if target == index { break }
+            arr.swapAt(target, index)
+            index = target
+        }
+        
+        return value
+    }
+
+    
+    var isEmpty: Bool {
+        arr.isEmpty
+    }
+    
+    var count: Int {
+        arr.count
+    }
+    
+    var root: T? {
+        arr[0]
+    }
+}
+
+let fIO = FileIO()
+let n = fIO.readInt()
+var arr = Array(repeating: Array(repeating: 0, count: n), count: n)
+var h = Heap<Int>(compare: >)
+
+for i in 0..<n {
+    for j in 0..<n {
+        arr[i][j] = fIO.readInt()
+    }
+}
+
+outer: for i in (0..<n).reversed() {
+    var flag = true
+    
+    for j in 0..<n {
+        if h.count < n {
+            h.enheap(arr[i][j])
+            flag = false
+            continue
+        }
+        
+        guard let r = h.root else { break }
+        if r < arr[i][j] {
+            flag = false
+            _ = h.deheap()
+            h.enheap(arr[i][j])
+        }
+    }
+    
+    if flag { break outer }
+}
+print(h.deheap()!)
