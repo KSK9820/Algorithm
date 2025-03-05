c18336a8c2774d751bb98d384b474f15d88da47c
Sukyoung Kim <68066104+KSK9820@users.noreply.github.com>
2025-03-06 00:21:45 +0900

    [Silver III] Title: 01타일, Time: 36 ms, Memory: 81588 KB -BaekjoonHub

---
+++ 백준/Silver/1904. 01타일/01타일.swift
@@ -0,0 +1,10 @@
+let n = Int(readLine()!)!
+var result = [0,1,2]
+if n <= 2 {
+    print(result[n])
+} else {
+    for i in 3...n {
+        result.append((result[i-2] + result[i-1]) % 15746)
+    }
+    print(result[n])
+}
