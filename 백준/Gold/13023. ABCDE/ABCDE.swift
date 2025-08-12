23f7f3d7bb5fff5f6ad6f33f9acea992a7bfab3a
Sukyoung Kim <68066104+KSK9820@users.noreply.github.com>
2025-08-13 00:24:53 +0900

    [Gold V] Title: ABCDE, Time: 772 ms, Memory: 69252 KB -BaekjoonHub

---
+++ 백준/Gold/13023. ABCDE/ABCDE.swift
@@ -0,0 +1,34 @@
+let nm = readLine()!.split(separator: " ").map { Int($0)! }
+let (n,m) = (nm[0], nm[1])
+var f = [[Int]](repeating: [Int](), count: n)
+for _ in 0..<m {
+    let ab = readLine()!.split(separator: " ").map { Int($0)! }
+    f[ab[0]].append(ab[1])
+    f[ab[1]].append(ab[0])
+}
+
+var result = 0
+var v = Array(repeating: false, count: n)
+var friend = Array(repeating: 0, count: 5)
+func dfs(_ idx: Int, depth: Int) {
+    if depth == 5 {
+        result = 1
+        return
+    }
+    
+    for j in f[idx] {
+        if v[j] { continue }
+        v[j] = true
+        friend[depth] = j
+        dfs(j, depth: depth + 1)
+        v[j] = false
+    }
+}
+
+for i in 0..<n where result != 1 {
+    v[i] = true
+    friend[0] = i
+    dfs(i, depth: 1)
+    v[i] = false
+}
+print(result)
